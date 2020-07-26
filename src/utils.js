5aefc485efcbfc5182ef07fb83d330ca8c1bfa78
gargar77 <gbautista2@knights.ucf.edu>
2020-07-26 09:44:31 -0400

    created an inheratence and vel vector ustil

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,22 @@
 // Utility code, especially vector math stuff.
+
+const Util = {
+    inherits: function(parent,child) {
+        let Surrogate = function() {};
+        Surrogate.prototype = parent.prototype;
+        child.prototype = new Surrogate();
+        child.prototype.constructor = child;
+    },
+
+    randomVec(length) {
+        const deg = 2 * Math.PI * Math.random();
+        return Util.scale([Math.sin(deg), Math.cos(deg)], length);
+      },
+      // Scale the length of a vector by the given amount.
+      scale(vec, m) {
+        return [vec[0] * m, vec[1] * m];
+      }
+};
+
+
+module.exports = Util;
